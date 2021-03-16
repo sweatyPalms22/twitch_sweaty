9bfd3b7b79b3718a92658cc3440c34d2d8b03d66
JustinBarsalou <justinbarsalou@gmail.com>
2021-03-15 19:12:50 -0500

    added embed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <ReactPlayer
+        url="https://www.twitch.tv/sweaty_palms_22"
+        controls
+      />
+
     </div>
   );
 }
